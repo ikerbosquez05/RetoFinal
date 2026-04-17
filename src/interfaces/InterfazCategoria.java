803f46fac7848d463b785c06c819e3dbf8a6e678
Naia&Marcela <dianaia2621@gmail.com>
2026-04-17 08:58:18 +0200

    Enhance documentation in InterfazCategoria interface
    Added comments to methods for better documentation.

--- src/interfaces/InterfazCategoria.java
+++ src/interfaces/InterfazCategoria.java
@@ -6,8 +6,14 @@ import clases.Categoria;
 public interface InterfazCategoria {
 
 	List<Categoria> listarCategorias() throws Exception;
+	// Método para obtener todas las categorías desde la base de datos.
+    // Devuelve una lista de objetos Categoria.
 
 	void insertarCategoria(Categoria c) throws Exception;
-
+	// Método para insertar una nueva categoría en la base de datos.
+    // Recibe un objeto Categoria como parámetro.
+	
 	void borrarCategoria(int id) throws Exception;
+	// Método para eliminar una categoría por su ID.
+    // No devuelve nada, solo ejecuta la eliminación.
 }
